b111a39ebf401cb0ae21d2394f2382d59fda64b7
LeeKennedy <mail@leekennedy.id.au>
2016-02-21 13:26:32 +1100

    Second flush pruning

--- reports/srm.markdown.R
+++ reports/srm.markdown.R
@@ -4,7 +4,7 @@ data.raw <- read.csv("data/VITB6.csv", as.is=TRUE, header=TRUE)
 srm_report <- function (x, max.pts = 100, points = 20) {
         library(rmarkdown)
         code <- substr(x$ANALYSIS[1],1,6)
-        rmarkdown::render("Control_Chart_Review.Rmd", output_file = paste(code, " Control Chart Review", ".html", sep=""))
+        rmarkdown::render("reports/Control_Chart_Review.Rmd", output_file = paste(code, " Control Chart Review", ".docx", sep=""))
 }
 
 
